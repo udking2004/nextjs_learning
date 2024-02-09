f1e6367b388ed48d5c92baf5a93b54bddca17d94
udit <uditsaxena940@gmail.com>
2024-02-09 15:16:19 +0530

    Initial commit

---
+++ app/components/AddtoCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddtoCart = () => {
+  return (
+    <div>
+      <button onClick={()=>{console.log("Add to card")}}>Add to cart</button>
+    </div>
+  )
+}
+
+export default AddtoCart
