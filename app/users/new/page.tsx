00cd4dafc77358b05a2dd5a52ed1cdf1955225ff
udit <uditsaxena940@gmail.com>
2024-02-09 15:37:35 +0530

    second commit

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const NewUserPage = () => {
+interface Users{ 
+  id: number;
+  name: string;
+}
+
+const NewUserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users",{next: { revalidate:10 }});
+  const users: Users[] = await res.json();
   return (
-    <div>
-      NewUserPage
-    </div>
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user=><li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
